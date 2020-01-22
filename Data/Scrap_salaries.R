af740664898e2af586dfd247835aab397d8f041c
joemarlo <joe.marlo.09@gmail.com>
2020-01-22 12:42:53 -0500

    Finalized the salary scraping and merged with the original dataset

--- Data/Scrap_salaries.R
+++ Data/Scrap_salaries.R
@@ -1,8 +1,5 @@
 library(tidyverse)
 library(httr)
-library(hms)
-library(lubridate)
-library(DescTools)
 library(stringdist)
 library(rvest)    
 library(stringr)   
@@ -73,6 +70,7 @@ head(salary.df)
 table(duplicated(salary.df))
 dim(salary.df)[1] == length(unique(salary.df$Player))
 
+
 # Match the names to the stats dataframe ----------------------------------
 
 # core problem is that we need to join the salary data with the stats data
@@ -81,18 +79,84 @@ dim(salary.df)[1] == length(unique(salary.df$Player))
 # read in the stats data
 stats.df <- read_csv('Data/season_stats_clean.csv')
 
-# remove extraneous columns
-stats.df <- stats.df[, c("Player", "Tm", "Pos")]
-names(stats.df)[2] <- "Team"
+# how many players are an exact match in the salary data? 
+table(stats.df$Player %in% salary.df$Player)
 
-unique(stats.df$Team)
+# which players are not in the salary data
+stats.df[!(stats.df$Player %in% salary.df$Player),] %>% View()
+
+# other data inspections
+unique(stats.df$Tm)
+length(unique(stats.df$Player))
+length(unique(salary.df$Player))
 table(salary.df$Player %in% stats.df$Player)
 unique(stats.df$Pos) %in% unique(salary.df$Pos)
 unique(salary.df$Pos) %in% unique(stats.df$Pos) 
-
 length(unique(salary.df$Team))
-length(unique(stats.df$Team))
+length(unique(stats.df$Tm))
 
+get_top_matches <- function(current.name, names.to.match, n = 5){
+  # function returns that top n matches of the old.name
+  #   within the new.names list via fuzzy string matching
+  
+  scores <- stringsim(current.name, names.to.match, method = "osa")
+  names.to.match[rev(order(scores))][1:n]
+}
+
+# test the function
+get_top_matches(stats.df$Player[1], salary.df$Player)
+
+# apply the function across the entire list to generate a data.frame
+#  containing the current.name and it's top 5 best matches
+all.matches <- lapply(stats.df$Player, get_top_matches, names.to.match = salary.df$Player) %>% 
+  unlist() %>% 
+  matrix(ncol = 5, byrow = TRUE) %>% 
+  as_tibble() %>% 
+  bind_cols(Current.name = stats.df$Player, .) %>%
+  setNames(c("Current.name", paste0("Match.", 1:5))) %>% 
+  mutate(Index = row_number())
+
+# return just the names that don't match along with their best matches 
+unmatched <- all.matches[all.matches$Current.name != all.matches$Match.1,]
+
+# visually inspect then manually adjust the names so they do match
+View(unmatched)
+
+# this are the indices of the ones to adjust
+# 1:7 2
+# 8 3
+# 9:12 2
+# 13:16 2
+# 17 # unsure, who is Gary Neal???????
+# 18:27 2
+
+# create a new column Matched.name and fill it with the corrected name
+unmatched$Matched.name <- NA
+unmatched[c(1:7, 9:12, 13:16, 18:27), "Matched.name"] <- unmatched[c(1:7, 9:12, 13:16, 18:27), "Match.1"]
+unmatched[8, "Matched.name"] <- unmatched[8, "Match.2"] 
+
+# create a new column New.name containing all the matched names
+all.matches$New.name <- all.matches$Current.name
+all.matches$New.name[all.matches$Index %in% unmatched$Index] <- unmatched$Matched.name
+
+# add the salary to the original stats.df dataframe
+stats.df$New.name <- all.matches$New.name
+stats.df$Salary <-  salary.df$Salary[match(stats.df$New.name, salary.df$Player)]
+stats.df <- select(stats.df, -New.name)
+
+# visually check if the top salaries seem correct
+stats.df %>% 
+  select(Player, Salary) %>% 
+  arrange(desc(Salary))
+
+# write the final data.frame to csv
+write.csv(stats.df, 'Data/season_stats_clean.csv', row.names = FALSE)
+
+
+
+
+
+# everything past here is old code and will be deleted ------------------------------
 
 
 # subway method -----------------------------------------------------------
